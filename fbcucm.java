854de073f1684f82f63bc048af5da8c660ced6f8
Karthick291998 <165255079+Karthick291998@users.noreply.github.com>
2024-06-21 19:59:53 +0530

    stepdefinition class facebook

--- fbcucm.java
+++ fbcucm.java
@@ -9,19 +9,20 @@ import org.openqa.selenium.WebElement;
 
 import basement.base;
 import basement.browser;
+import fb.ObjectRespository.fbAccountCreation;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class fbcucm extends base {
+public class fbcucm extends fbAccountCreation {
 	String propertypath="C:\\Users\\karth\\eclipse-workspace\\cuccumber\\src\\test\\resources\\utility.property";
 	
 	public static base b=new base();
+	public static fbAccountCreation fb=new fbAccountCreation();
 	@Given("Launch the browserr")
 	public void launch_the_browserr()
 	{
 		b.setup(browser.CHROME,"https://www.facebook.com/r.php?next=https%3A%2F%2Fwww.facebook.com%2F&locale=en_GB&display=page" );
-		
 	}
 	
 	@When("Enter the firstname {string}")
@@ -30,13 +31,15 @@ public class fbcucm extends base {
 		String name=b.readproperty(propertypath, "firstname");
 		WebElement firstname=b.element("xpath", name);
 		b.type(firstname, string);
+		//b.type(fb.getFirstname(), string);
 	}
 
 	@When("Enter the lastname {string}")
 	public void enter_the_lastname(String string) {
 		  //last name
-		WebElement lastname=b.element("xpath", "//input[@name='lastname']");
-		b.type(lastname, string);
+		//WebElement lastname=b.element("xpath", "//input[@name='lastname']");
+	//	b.type(lastname, string);
+		b.type(fb.getLastname(), string);
 	}
 
 	@When("Enter the email {string}")
@@ -44,6 +47,7 @@ public class fbcucm extends base {
 		 //email or num
 		WebElement email=b.element("xpath", "//input[@name='reg_email__']");
 		b.type(email, string);
+		//b.type(fb.getEmail(), string);
 	}
 
 	@When("Enter the new password {string}")
@@ -51,6 +55,7 @@ public class fbcucm extends base {
 		 //password
 		WebElement pass=b.element("xpath", "//input[@name='reg_passwd__']");
 		b.type(pass, string);
+		//b.type(fb.getPassword(), string);
 	}
 
 	@When("Enter the dateOfBirth {string}")
@@ -58,11 +63,14 @@ public class fbcucm extends base {
 		  String date=string;
 		  String[] str=date.split(" ");
 		  WebElement day=b.element("xpath","//select[@id='day']");
-		  b.dropdown(day,str[0]);
+		  b.type(day, str[0]);
+		 // b.dropdown(fb.getDay(),str[0]);
 		  WebElement month=b.element("xpath","//select[@id='month']");
-		  b.dropdown(month,str[1]);
+		  b.type(month, str[1]);
+		//  b.dropdown(fb.getMonth(),str[1]);
 		  WebElement year=b.element("xpath","//select[@id='year']");
-		  b.dropdown(year,str[2]);
+		  b.type(year, str[2]);
+		//  b.dropdown(fb.getYear(),str[2]);
 	}
 
 	@When("Enter the gender {string}")
@@ -71,19 +79,23 @@ public class fbcucm extends base {
 		  if(gender.equals("male"))
 		  {
 			b.click(b.element("xpath","//label[text()='Male']"));
+			 // b.click(fb.getMale());
 				  }
 		  else if(gender.equals("female"))
 		  {
 			  b.click(b.element("xpath","//label[text()='Female']"));
+			 // b.click(fb.getFemale());
 		  }
 		  else
 		  {
 			  b.click(b.element("xpath","//label[text()='Custom']"));
+			 // b.click(fb.getCustom());
 		  }
 	}
 	@Then("click the signup {string}")
 	public void click_the_signup(String string) throws InterruptedException, IOException {
 		b.click(b.element("xpath","//button[@name='websubmit']"));
+		//b.click(fb.getsignup());
 		Thread.sleep(2000);
 		b.screenshot(string);
 	}
